c7f836ae94feec7645413644847742dcf4932639
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-12-06 17:39:38 +0000

    Taken input from API generator

--- src/Components/Misdemeanour.js
+++ src/Components/Misdemeanour.js
@@ -1,52 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import generateMisdemeanours from "../Docs/generate_misdemeanours"
 import MisdemeanourCard from "./MisdemeanourCard";
 import "./Misdemeanour.css";
 
 const Misdemeanour = () => {
-   const [Misdemeanour, setMisdemeanour] = useState([generateMisdemeanours()]);
-   const obj = [{
-      citizenId: 19739,
-      date: "12/5/2021",
-      misdemeanour: "lift"
-   },
-   {
-      citizenId: 1455,
-      date: "12/5/2021",
-      misdemeanour: "rudeness"
-   },
-   {
-      citizenId: 1783,
-      date: "12/5/2021",
-      misdemeanour: "united"
-   },
-   {
-      citizenId: 10825,
-      date: "12/5/2021",
-      misdemeanour: "vegetables"
-   },
-   {
-      citizenId: 130,
-      date: "12/5/2021",
-      misdemeanour: "rudeness"
+   const [misdemeanour, setMisdemeanour] = useState([]);
+
+   useEffect(() => {
+      getData()
+   }, [])
+
+   const getData = async () => {
+      const response = await generateMisdemeanours();
+      setMisdemeanour(response);
    }
 
-   ];
-   
+   console.log("misdemeanour", misdemeanour);
+
    return (
       <div>
          <h3>Misdemeanour</h3>
-         <table> 
+         <table>
             <thead>
-            <tr >
-               <th style={{width: '25%'}}>Citizen ID </th>
-               <th style={{width: '25%'}}>Date </th>
-               <th style={{width: '25%'}}> Misdemeanour </th>
-               <th style={{width: '25%'}}> Punishment Idea </th>
-            </tr>
+               <tr >
+                  <th style={{ width: '25%' }}>Citizen ID </th>
+                  <th style={{ width: '25%' }}>Date </th>
+                  <th style={{ width: '25%' }}> Misdemeanour </th>
+                  <th style={{ width: '25%' }}> Punishment Idea </th>
+               </tr>
             </thead>
             <tbody>
-               {obj.map(o => {
+               {misdemeanour.map(o => {
                   return <MisdemeanourCard key={o.citizenId} citizenId={o.citizenId}
                      date={o.date}
                      misdemeanour={o.misdemeanour}
